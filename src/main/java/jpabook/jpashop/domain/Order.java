82d926efaf93573ce024aa16ad06bb38dcaffbc7
GRIFFITHH <glqglq032@gmail.com>
2021-07-19 22:29:22 +0900

    SpringBoot & JPA - 주문,주문상품 엔티티 , 주문 리포지토리 구현 완료

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -7,6 +10,7 @@ import java.util.List;
 
 @Entity
 @Table(name ="orders")
+@Getter @Setter
 public class Order {
 
     @Id @GeneratedValue
@@ -28,4 +32,64 @@ public class Order {
 
     @Enumerated(EnumType.STRING) //무조건 스트링으로 써야함!
     private OrderStatus orderStatus; //주문상태 [ORDER , CANCEL]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    //생성메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate((LocalDateTime.now()));
+        return order;
+    }
+
+    //비즈니스 로직
+    //**주문취소
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setOrderStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //조회 로직
+
+    /**
+     * 전체 주문 가격 조회회
+    * @return
+     */
+
+//    public int getTotalPrice() {
+//        int totalPrice = 0;
+//        for (OrderItem orderItem : orderItems) { //향상된 for 문
+//            totalPrice += orderItem.getTotalPrice();
+//        }
+//        return totalPrice;
+//
+//    } 아래로 코드를 바꾼것 , Stream 배우면 다시보자!!
+    public int getTotalPrice() {
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+    }
 }
